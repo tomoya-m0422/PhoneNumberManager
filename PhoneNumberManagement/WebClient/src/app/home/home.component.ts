0c76bd3cb47bd7d4412e04b5c7cd59472d90d164
tomoya-m0422 <tomoya.mukaihara@mjs.co.jp>
2022-09-13 18:28:56 +0900

    2022/09/13 18:28

--- PhoneNumberManagement/WebClient/src/app/home/home.component.ts
+++ PhoneNumberManagement/WebClient/src/app/home/home.component.ts
@@ -10,26 +10,27 @@ import { ManagementPerson } from './home.viewmodel';
 })
 export class HomeComponent implements OnInit {
 
-  public managementPerson: ManagementPerson[] = [];
+  managementPerson: ManagementPerson[] = [];
 
   constructor(private router: Router) {
-    this.managementPerson = this.managementPerson;
+    this.managementPerson = this.managementPerson
    }
 
-
   ngOnInit(): void {
+    let managementPerson: ManagementPerson[] = [];
     //#region 初期処理
     $(function () {
       $.ajax(
         {
-          async: true,
+          async: false,
           url: "https://localhost:7059/Management",
           type: "get",
-          dataType: "json"
+          dataType: "json",
 
         }
       )
         .done(function (data) {
+          alert("done")
           //A-1.検索欄の初期処理
           $.each(data,function(index,item){
             $("#NameList").append
@@ -42,19 +43,29 @@ export class HomeComponent implements OnInit {
           //A-2.一覧表示の初期処理
           //これが難しい
           $.each(data,function(index,item){
-            managementPerson.push(item)
+            managementPerson.push({
+              StaffNumber: item.staffNumber,
+              StaffName: item.staffName,
+              CompanyID: item.companyID,
+              CompanyName: item.companyName,
+              DepartmentID: item.departmentID,
+              DepartmentName: item.departmentName,
+              ExtensionNumber: item.extensionNumber,
+              Memo: item.memo
+            })
+            hoge(managementPerson)
+            var aaa = 0;
+            moge(aaa);
           }
           )
-
           //TS-1.詳細表示の処理
           $(".Name , .button").on('click',function(){
             var staffNumber =  $(this).data('id');
             //HomeComponent.router.navigateByUrl("./../person-create")
           });
-
-
         }
         )
+
         .fail(function () {
           window.alert("ERROR:データベースと接続できませんでした");
           $("#table").append
@@ -63,14 +74,35 @@ export class HomeComponent implements OnInit {
           )
         }
         )
+
+      function hoge(managementPerson: ManagementPerson){
+        this.managementPerson = managementPerson
+      }
+
     }
     )
     //#endregion
-
+    function moge(managementPerson: ManagementPerson[]){
+      this.managementPerson = managementPerson;
+    }
+    this.managementPerson = managementPerson;
   }
 
+  /*
+  hoge(item: any) : void {
+    let res: ManagementPerson = {
+      StaffNumber: item.staffNumber,
+      StaffName: item.staffName,
+      CompanyID: item.CompanyID,
+      CompanyName: item.CompanyName,
+      DepartmentID: item.DepartmentID,
+      DepartmentName: item.DepartmentName,
+      ExtensionNumber: item.ExtensionNumber,
+      Memo: item.Memo
+    }
+
+    this.managementPerson.push(res);
+  }
 
 }
-
-
-
+*/
--- PhoneNumberManagement/WebClient/src/app/home/home.viewmodel.ts
+++ PhoneNumberManagement/WebClient/src/app/home/home.viewmodel.ts
@@ -1,12 +1,60 @@
+export class ManagementPerson {
+  constructor(
+    StaffNumber: number,
+    StaffName: string,
+    CompanyID: number,
+    DepartmentID: number,
+    ExtensionNumber: string,
+    Memo: string,
+    CompanyName: string,
+    DepartmentName: string
+  ) {
+    this.StaffNumber = StaffNumber;
+    this.StaffName = StaffName;
+    this.CompanyID = CompanyID;
+    this.DepartmentID = DepartmentID;
+    this.ExtensionNumber = ExtensionNumber;
+    this.Memo = Memo;
+    this.CompanyName = CompanyName;
+    this.DepartmentName = DepartmentName;
+  }
+  public StaffNumber: number;
+  public StaffName: string;
+  public CompanyID: number;
+  public DepartmentID: number;
+  public ExtensionNumber: string;
+  public Memo: string;
+  public CompanyName: string;
+  public DepartmentName: string;
+}
+//datamodelBaseを継承しないといけない
 
-export interface ManagementPerson{
-  StaffNumber: number;
-  StaffName: string;
-  CompanyID: number;
-  DepartmentID: number;
-  ExtensionNumber: string;
-  Memo: string;
-  CompanyName: string;
-  DepartmentName: string;
+/*
+//Jsonをmodelにする
+class DataModelBase {
+  /**
+   * Jsonからモデルのインスタンスを生成する
+   * @param jsonArray
+   * @param cls
+   */
+  /*
+  public static fillFromJsons<T extends DataModelBase>(
+    jsonArray: Array<JSON>,
+    cls: typeof DataModelBase
+  ): Array<T> {
+    var array: Array<DataModelBase> = new Array<DataModelBase>();
+    var dataCount = jsonArray == null ? 0 : jsonArray.length;
+    for (var i = 0; i < dataCount; i++) {
+      var json = jsonArray[i];
+      var model = new cls();
+      for (var propName in json) {
+        model[propName as keyof DataModelBase] = json[propName as keyof JSON];
+        //model[keyof typeof propName] = json[keyof typeof propName];
+      }
+      array.push(model);
+    }
+    return <Array<T>>array;
+  }
 }
 
+*/
